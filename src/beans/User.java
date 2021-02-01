0c96a8e1f93fb44f3c9ae3bae9fd38c84f502a97
CHRISTIAN HILL <christian.hill.116@gmail.com>
2021-02-01 12:41:03 -0700

    Update User.java

--- src/beans/User.java
+++ src/beans/User.java
@@ -3,18 +3,19 @@ package beans;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.inject.Named;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 
  @ManagedBean 
 public class User {
 	public String userName;
 	public String password;
+	public String fullName;
+	public String email;
+	
+	
 	
 	public User() {
-		userName = "gcu123";
-		password = "password123";
+		
 	}
 
 	public String getUserName() {
@@ -31,6 +32,22 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 
 	
